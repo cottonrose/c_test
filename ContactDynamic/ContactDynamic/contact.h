6ef8ae3ca0fe4df00bd9875fc692bb13e06a5670
cottonrose <1320210577@qq.com>
2018-11-13 18:12:43 +0800

    提交代码

---
+++ ContactDynamic/ContactDynamic/contact.h
@@ -0,0 +1,36 @@
+#ifndef __CONTACT_H__
+#define __CONTACT_H__
+
+#define DEFAULT_SZ 3
+#define NAME_MAX 20
+#define SEX_MAX 6
+#define NUM_MAX 20
+#define ADR_MAX 30
+
+typedef struct PeoInfo
+{
+	char name[NAME_MAX];
+	int age;
+	char sex[SEX_MAX];
+	char num[NUM_MAX];
+	char adr[ADR_MAX];
+}PeoInfo;
+typedef struct Contact
+{
+	PeoInfo *data;
+	int sz;
+	int capacity;
+}Contact,*pContact;
+
+void InitContact(pContact p);
+void DestoryContact(pContact p);
+
+void AddContact(pContact p);
+void ShowContact(pContact p);
+void SubContact(pContact p);
+void SearchContact(pContact p);
+void ModifyContact(pContact p);
+void EmptyContact(pContact p);
+void SortContact(pContact p);
+
+#endif //__CONTACT_H__
---
+++ ContactDynamic/ContactDynamic/test.c
@@ -0,0 +1,74 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include "contact.h"
+
+void menu()
+{
+	printf("*******************************\n");
+	printf("***    1.Add      2.Sub    ***\n");
+	printf("***    3.Search   4.Sort   ***\n");
+	printf("***    5.Show     6.Modify ***\n");
+	printf("***    7.Empty    0.Exit   ***\n");
+	printf("*******************************\n");
+}
+enum Select
+{
+	EXIT,
+	ADD,
+	SUB,
+	SEARCH,
+	SORT,
+	SHOW,
+	MODIFY,
+	EMPTY
+};
+
+void test()
+{
+	int input = 0;
+	Contact con;
+	InitContact(&con);
+	do
+	{
+		menu();
+		printf("ÇëÑ¡Ôñ£º\n");
+		scanf("%d",&input);
+		switch(input)
+		{
+		case ADD:
+			AddContact(&con);
+			break;
+		case SUB:
+			SubContact(&con);
+			break;
+		case SEARCH:
+			SearchContact(&con);
+			break;
+		case SORT:
+			SortContact(&con);
+			break;
+		case SHOW:
+			ShowContact(&con);
+			break;
+		case MODIFY:
+			ModifyContact(&con);
+			break;
+		case EMPTY:
+			EmptyContact(&con);
+			break;
+		case EXIT:
+			DestoryContact(&con);
+			printf("ÍË³ö\n");
+			break;
+		default:
+			break;
+		}
+	}while(input);
+}
+
+int main()
+{
+	test();
+	return 0;
+}
